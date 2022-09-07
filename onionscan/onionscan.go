6dac492a74a5f001ead1d4400338b4282edc5cc1
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 15:21:11 -0500

    Update onionscan.go
    Update repository name

--- onionscan/onionscan.go
+++ onionscan/onionscan.go
@@ -2,9 +2,9 @@ package onionscan
 
 import (
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/protocol"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/protocol"
+	"github.com/Jagsec/onionscan/report"
 	"time"
 )
 
