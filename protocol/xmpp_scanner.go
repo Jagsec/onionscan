82fa22129856443717b835e6e656afee0eeb45eb
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 16:00:43 -0500

    Update xmpp_scanner.go
    Update repo name

--- protocol/xmpp_scanner.go
+++ protocol/xmpp_scanner.go
@@ -2,9 +2,9 @@ package protocol
 
 import (
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/report"
+	"github.com/Jagsec/onionscan/utils"
 )
 
 type XMPPProtocolScanner struct {
