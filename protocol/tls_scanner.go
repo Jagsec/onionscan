2debd9fe52d91ee9de06921ce90ee72b4cde425e
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 15:57:59 -0500

    Update tls_scanner.go
    Update repo name

--- protocol/tls_scanner.go
+++ protocol/tls_scanner.go
@@ -3,9 +3,9 @@ package protocol
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/report"
+	"github.com/Jagsec/onionscan/utils"
 )
 
 type TLSProtocolScanner struct {
