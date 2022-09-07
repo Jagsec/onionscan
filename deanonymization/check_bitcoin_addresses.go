7ca002e3664e2f70f3a94c20ec48991e985f6c71
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 15:11:33 -0500

    Update check_bitcoin_addresses.go
    Update repository name

--- deanonymization/check_bitcoin_addresses.go
+++ deanonymization/check_bitcoin_addresses.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"errors"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/report"
 	"regexp"
 	"strings"
 )
