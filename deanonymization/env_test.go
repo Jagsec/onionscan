3080f128d050b48103335aedd04326fbc4432421
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 15:16:27 -0500

    Update env_test.go
    Update repository name

--- deanonymization/env_test.go
+++ deanonymization/env_test.go
@@ -1,10 +1,10 @@
 package deanonymization
 
 import (
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/crawldb"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/crawldb"
+	"github.com/Jagsec/onionscan/model"
+	"github.com/Jagsec/onionscan/report"
 	"io/ioutil"
 	"net/http"
 	"os"
