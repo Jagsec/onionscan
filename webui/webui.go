fec8d88f1374fa35612f75a2124ffd10c08b05a4
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 16:05:28 -0500

    Update webui.go
    Update repo name

--- webui/webui.go
+++ webui/webui.go
@@ -3,9 +3,9 @@ package webui
 import (
 	"errors"
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/crawldb"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/crawldb"
+	"github.com/Jagsec/onionscan/utils"
 	"html/template"
 	"log"
 	"net/http"
