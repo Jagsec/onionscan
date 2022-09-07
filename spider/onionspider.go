4deec2c7bc29310495dc9f81b0bbc059c024b93b
Jagsec <62567044+Jagsec@users.noreply.github.com>
2022-09-07 16:02:28 -0500

    Update onionspider.go
    Update repo name

--- spider/onionspider.go
+++ spider/onionspider.go
@@ -3,9 +3,9 @@ package spider
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Jagsec/onionscan/config"
+	"github.com/Jagsec/onionscan/model"
+	"github.com/Jagsec/onionscan/report"
 	"golang.org/x/net/proxy"
 	"net/http"
 	"net/http/cookiejar"
